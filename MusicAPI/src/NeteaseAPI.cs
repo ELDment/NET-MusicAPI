4ab94de12376fc92104960a89956ebbb82ad0778
ELDment <eldment@qq.com>
2025-01-25 21:31:54 +0800

    🎉 更新
    🎉 更新

--- MusicAPI/src/NeteaseAPI.cs
+++ MusicAPI/src/NeteaseAPI.cs
@@ -4,19 +4,17 @@ using System.Security.Cryptography;
 using System.Text;
 
 namespace MusicAPI;
-public class NeteaseAPI : IMusicAPI
-{
+public class NeteaseAPI : IMusicAPI {
 
-	public Dictionary<string, string> Cookies = new();
+	public Dictionary<string, string> Headers = new();
 
 	private HttpClient HttpClient = new();
 
-	private Dictionary<string, string> GetHeader()
-	{
-		string cookieString = string.Join("; ", Cookies.Select(cookie => $"{cookie.Key}={cookie.Value}"));
-		return new Dictionary<string, string> {
+	private Dictionary<string, string> GetHeader() {
+		//string cookieString = string.Join("; ", Cookies.Select(cookie => $"{cookie.Key}={cookie.Value}"));
+		Dictionary<string, string> keyValues = new Dictionary<string, string> {
 						{ "Referer", "https://music.163.com/" },
-						{ "Cookie", "appver=8.2.30; os=iPhone OS; osver=15.0; EVNSM=1.0.0; buildver=2206; channel=distribution; machineid=iPhone13.3; "+cookieString },
+						{ "Cookie", "appver=8.2.30; os=iPhone OS; osver=15.0; EVNSM=1.0.0; buildver=2206; channel=distribution; machineid=iPhone13.3" },
 						{ "User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 CloudMusic/0.1.1 NeteaseMusic/8.2.30" },
 						{ "X-Real-IP", HttpManager.GetRandomIP() },
 						{ "Accept", "*/*" },
@@ -24,10 +22,17 @@ public class NeteaseAPI : IMusicAPI
 						{ "Connection", "keep-alive" },
 						{ "Content-Type", "application/x-www-form-urlencoded" }
 					};
+
+		foreach (var header in Headers) {
+			keyValues.TryAdd(header.Key, header.Value);
+			if (keyValues.ContainsKey(header.Key))
+				keyValues[header.Key] = header.Value;
+			//Console.WriteLine($"{keyValues[header.Key]}: {keyValues[header.Value]}");
+		}
+		return keyValues;
 	}
 
-	public async Task<List<Song>?> Search(string keyword, ISearchParamBuilder? builder = null)
-	{
+	public async Task<List<Song>?> Search(string keyword, ISearchParamBuilder? builder = null) {
 		builder = builder ?? new NeteaseSearchParamBuilder();
 		NeteaseSearchParam param = (NeteaseSearchParam)builder.Build();
 		Dictionary<string, object> apiRequest = new Dictionary<string, object> {
@@ -49,16 +54,13 @@ public class NeteaseAPI : IMusicAPI
 			return null;
 
 		List<Song> searchResult = new();
-		foreach (dynamic? jsonData in json!.result!.songs)
-		{
+		foreach (dynamic? jsonData in json!.result!.songs) {
 			if (jsonData == null)
 				continue;
 			var artists = new List<Artist>();
 
-			foreach (dynamic? artist in jsonData!.ar)
-			{
-				artists.Add(new Artist
-				{
+			foreach (dynamic? artist in jsonData!.ar) {
+				artists.Add(new Artist {
 					id = artist.id,
 					name = artist.name
 				});
@@ -90,8 +92,7 @@ public class NeteaseAPI : IMusicAPI
 		return searchResult;
 	}
 
-	public async Task<Song?> GetSong(string id)
-	{
+	public async Task<Song?> GetSong(string id) {
 		Dictionary<string, object> apiRequest = new Dictionary<string, object> {
 				{ "url", "http://music.163.com/api/v3/song/detail/" },
 				{ "body", new Dictionary<string, object> {
@@ -106,28 +107,23 @@ public class NeteaseAPI : IMusicAPI
 		if (string.IsNullOrWhiteSpace(jsonReasult) || json == null)
 			return null;
 
-		foreach (dynamic? jsonData in json!.songs)
-		{
+		foreach (dynamic? jsonData in json!.songs) {
 			if (jsonData == null)
 				continue;
 
 			List<Artist> artists = new List<Artist>();
-			foreach (dynamic? artist in jsonData!.ar)
-			{
-				artists.Add(new Artist
-				{
+			foreach (dynamic? artist in jsonData!.ar) {
+				artists.Add(new Artist {
 					id = artist.id,
 					name = artist.name
 				});
 			}
-			return new NeteaseSong
-			{
+			return new NeteaseSong {
 				Api = this,
 				id = jsonData!.id,
 				name = jsonData!.name,
 				artists = artists,
-				album = new Album
-				{
+				album = new Album {
 					id = jsonData!.al!.id,
 					name = jsonData!.al!.name,
 					pic = jsonData!.al!.pic
@@ -145,8 +141,7 @@ public class NeteaseAPI : IMusicAPI
 		return null;
 	}
 
-	public async Task<SongResource?> GetSongResource(string id, IFetchResourceParamBuilder? builder = null)
-	{
+	public async Task<SongResource?> GetSongResource(string id, IFetchResourceParamBuilder? builder = null) {
 		builder = builder ?? new NeteaseFetchResourceParamBuilder();
 		NeteaseFetchResourceParam param = (NeteaseFetchResourceParam)builder.Build();
 		Dictionary<string, object> apiRequest = new Dictionary<string, object> {
@@ -189,8 +184,7 @@ public class NeteaseAPI : IMusicAPI
 		return resource;
 	}
 
-	public async Task<Lyric?> GetLyric(string id)
-	{
+	public async Task<Lyric?> GetLyric(string id) {
 		Dictionary<string, object> apiRequest = new Dictionary<string, object> {
 				{ "url", "http://music.163.com/api/song/lyric" },
 				{ "body", new Dictionary<string, object> {
@@ -227,15 +221,13 @@ public class NeteaseAPI : IMusicAPI
 }
 
 
-public static class NeteaseCrypto
-{
+public static class NeteaseCrypto {
 	private static readonly string Modulus = "157794750267131502212476817800345498121872783333389747424011531025366277535262539913701806290766479189477533597854989606803194253978660329941980786072432806427833685472618792592200595694346872951301770580765135349259590167490536138082469680638514416594216629258349130257685001248172188325316586707301643237607";
 	private static readonly string PubKey = "65537";
 	private static readonly string Nonce = "0CoJUm6Qyw8W8jud";
 	private static readonly string IV = "0102030405060708";
 
-	public static NeteaseEncryptedResult NetEaseAESCBC(Dictionary<string, object> api)
-	{
+	public static NeteaseEncryptedResult NetEaseAESCBC(Dictionary<string, object> api) {
 		string skey = GetRandomHex(16) ?? "B3v3kH4vRPWRJFfH";
 
 		string body = JsonConvert.SerializeObject(api["body"]);
@@ -256,18 +248,15 @@ public static class NeteaseCrypto
 		return encryptedResult;
 	}
 
-	private static string GetRandomHex(int length)
-	{
-		using (var rng = RandomNumberGenerator.Create())
-		{
+	private static string GetRandomHex(int length) {
+		using (var rng = RandomNumberGenerator.Create()) {
 			byte[] bytes = new byte[length / 2];
 			rng.GetBytes(bytes);
 			return BitConverter.ToString(bytes).Replace("-", "").ToLower();
 		}
 	}
 
-	private static string EncryptAES(string plainText, string key, string iv)
-	{
+	private static string EncryptAES(string plainText, string key, string iv) {
 		byte[] keyBytes = Encoding.UTF8.GetBytes(key);
 		byte[] ivBytes = Encoding.UTF8.GetBytes(iv);
 
@@ -276,8 +265,7 @@ public static class NeteaseCrypto
 		if (ivBytes.Length != 16)
 			throw new ArgumentException("AES-128-CBC的IV必须为16字节长");
 
-		using (Aes aes = Aes.Create())
-		{
+		using (Aes aes = Aes.Create()) {
 			aes.Key = keyBytes;
 			aes.IV = ivBytes;
 			aes.Mode = CipherMode.CBC;
@@ -285,10 +273,8 @@ public static class NeteaseCrypto
 
 			ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
 			byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
-			using (MemoryStream ms = new MemoryStream())
-			{
-				using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-				{
+			using (MemoryStream ms = new MemoryStream()) {
+				using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write)) {
 					cs.Write(plainBytes, 0, plainBytes.Length);
 					cs.FlushFinalBlock();
 				}
@@ -298,8 +284,7 @@ public static class NeteaseCrypto
 		}
 	}
 
-	private static string SecretKey(string skey, string pubkey, string modulus)
-	{
+	private static string SecretKey(string skey, string pubkey, string modulus) {
 		skey = ReverseString(skey);
 		skey = bchexdec(str2hex(skey));
 		skey = bcpowmod(skey, pubkey, modulus);
@@ -313,19 +298,16 @@ public static class NeteaseCrypto
 		return skey;
 	}
 
-	private static string ReverseString(string input)
-	{
+	private static string ReverseString(string input) {
 		char[] charArray = input.ToCharArray();
 		Array.Reverse(charArray);
 		return new string(charArray);
 	}
 
-	private static string bchexdec(string hex)
-	{
+	private static string bchexdec(string hex) {
 		BigInteger result = 0;
 		int length = hex.Length;
-		for (int i = 0; i < length; i++)
-		{
+		for (int i = 0; i < length; i++) {
 			char hexChar = hex[i];
 			BigInteger digitValue = HexCharToValue(hexChar);
 			BigInteger power = BigInteger.Pow(16, length - i - 1);
@@ -335,19 +317,16 @@ public static class NeteaseCrypto
 		return result.ToString();
 	}
 
-	private static string str2hex(string input)
-	{
+	private static string str2hex(string input) {
 		StringBuilder hexBuilder = new StringBuilder();
-		foreach (char c in input)
-		{
+		foreach (char c in input) {
 			string hexCode = ((int)c).ToString("X2");
 			hexBuilder.Append(hexCode);
 		}
 		return hexBuilder.ToString();
 	}
 
-	private static string bcpowmod(string input, string pubkey, string modulus)
-	{
+	private static string bcpowmod(string input, string pubkey, string modulus) {
 		BigInteger skeyBigInt = BigInteger.Parse(input);
 		BigInteger pubkeyBigInt = BigInteger.Parse(pubkey);
 		BigInteger modulusBigInt = BigInteger.Parse(modulus);
@@ -356,22 +335,14 @@ public static class NeteaseCrypto
 		return result.ToString();
 	}
 
-	private static BigInteger HexCharToValue(char hexChar)
-	{
-		if (hexChar >= '0' && hexChar <= '9')
-		{
+	private static BigInteger HexCharToValue(char hexChar) {
+		if (hexChar >= '0' && hexChar <= '9') {
 			return hexChar - '0';
-		}
-		else if (hexChar >= 'a' && hexChar <= 'f')
-		{
+		} else if (hexChar >= 'a' && hexChar <= 'f') {
 			return 10 + hexChar - 'a';
-		}
-		else if (hexChar >= 'A' && hexChar <= 'F')
-		{
+		} else if (hexChar >= 'A' && hexChar <= 'F') {
 			return 10 + hexChar - 'A';
-		}
-		else
-		{
+		} else {
 			throw new ArgumentException($"无效HEX字符: {hexChar}");
 		}
 	}
